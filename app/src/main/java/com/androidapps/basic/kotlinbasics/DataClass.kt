187a57ef93909110912d1342a98c2e7d040662c3
uma <uma2011ece@gmail.com>
2022-02-03 10:47:01 -0500

    Data class updated

--- app/src/main/java/com/androidapps/basic/kotlinbasics/DataClass.kt
+++ app/src/main/java/com/androidapps/basic/kotlinbasics/DataClass.kt
@@ -2,6 +2,20 @@ package com.androidapps.basic.kotlinbasics
 
 class DataClass
 
+/*it is the class  just to store data.only has getters and setters
+
+* ex:if we get data JSON format data from backend server,we need to store that data in this class to use further
+ in java ,we call this as POJO class.in kotlin ,we call this Data class
+ primary constructor has at least one parameter
+ all primary constructor parameters need to be var or val
+ can have body but not required
+
+ Automatically generated class methods:
+ equals() or == ->compare data not object reference
+ toString()  ->prints out variable names and data
+ copy()-> creates a copy with only with some data changed */
+
+
 /*To create the model class to hold data only ,we need to create data class
 * to create data class ,we need to add just data keyword in front of class keyword.
 * for the data class compiler derive some functionality for us like
@@ -37,30 +51,31 @@ class DataClass
 /*The compiler only uses the properties defined inside the primary constructor for the automatically generated functions.
         It excludes the properties that are declared in the class body*/
 
-/* fun main(args: Array<String>)
- {
-     //declaring a data class
-     data class man(val name: String)
-     {
-         //property declared in class body
-         var height: Int = 0;
-     }
+/*fun main(args: Array<String>) {
+    //declaring a data class
+    data class man(val name: String) {
+        //property declared in class body
+        var height: Int = 0;
+    }
 
-     //declaring a variable of the above data class and
-     //initializing values to all parameters
+    //declaring a variable of the above data class and
+    //initializing values to all parameters
 
-     val man1=man("manish")
-     //class body properties must be assigned uniquely
-     man1.height = 70
+    val man1 = man("manish")
+    //class body properties must be assigned uniquely
+    man1.height = 70
 
-     //this method prints the details of class that
-     //are declared in primary constructor
-     println(man1.toString());//Here height is not used by the toString() function
+    //this method prints the details of class that
+    //are declared in primary constructor
+    println(man1);//Here height is not used by the toString() function
 
-     //printing the height of man1
-     println(man1.height);
- }
-*/
+    //printing the height of man1
+    println(man1.height);
+
+
+    //when we print object of class using println(man1) ,we will get some unique code
+    //but if we print object of data class we will get value of that class parameters
+}*/
 
 
 /*copy()
@@ -142,14 +157,72 @@ fun main(args: Array<String>) {
     println("hash1 == hash 3 ${hash1.equals(hash3)}")
 
 
-    /*man1 and man2 have same object contents, so they are equal, thus they have same hash code values*/
+    /*man1 and man3 have same object contents, so they are equal, thus they have same hash code values*/
 
     /*Component functions generated for data classes make it possible to use them in destructuring declarations:*/
     val (name, age) = man1;
     println("name is $name and his age is $age")
 
 
+    // ==
+    val regularClassObject1 = RegularClass("someOne", 50)
+    var regularClassObject2 = RegularClass("someOne", 50)
+    println(regularClassObject1 == regularClassObject2)  //compare object references are equal or not(false)
+
+    var dataClassObject1 = DataClass1("someOne", 50)
+    var dataClassObject2 = DataClass1("someOne", 50)
+    println(dataClassObject1 == dataClassObject2)  //compare object values are equal or not(true)
+
+
+    //toString()
+    println(regularClassObject1)  //regular class object will print object code(toString is hidden here)
+    println(dataClassObject1)  //data class object will print information contained in that object
+
+    //copy()
+    //for regular class copy() is not available
+    println(dataClassObject1.toString())
+    var dataclassObject3 = dataClassObject1.copy(name = "rjg")
+    println(dataclassObject3.toString())
+
+
+    var article1FromServer=Article("title1","content1",2)
+    var article2FromServer=Article("title2","content2",4)
+    var updateArticle1=article1FromServer.copy("updatedTitle1")
+    var updateArticle2=article2FromServer.copy("updatedTitle2")
+    println(article1FromServer)
+    println(article2FromServer)
+    println(updateArticle1)
+    println(updateArticle2)
+
 }
 
 
+class RegularClass(
+    var name: String,
+    var age: Int
+)
 
+data class DataClass1(
+    var name: String,
+    var age: Int
+)
+
+
+/*Exercise: Data classes
+
+A network server responds with this information about blog articles
+
+    Title
+
+    Content
+
+    Number of readers
+
+You receive 2 articles from the server.
+
+Update the title to suit your blog but also keep the originals.
+
+Print out all the article information.*/
+
+
+data class Article(var title: String, var content: String, var numberOfReaders: Int)
